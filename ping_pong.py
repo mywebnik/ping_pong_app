c4796ea5cb18c5440548102628e732a55447d327
mywebnik <131589705+mywebnik@users.noreply.github.com>
2023-04-23 18:01:34 +0300

    Update ping_pong.py
    добавлен класс GameSprite, добавлен спрайт мяч

--- ping_pong.py
+++ ping_pong.py
@@ -7,6 +7,22 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+#класс который будем ипользовать для всех спрайтов
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 7)
+
 #флаги, отвечающие за состояние игры
 game = True
 finish = False
@@ -20,6 +36,7 @@ while game:
 
    if finish != True:
        window.fill(back)
+       ball.reset()
 
    display.update()
    clock.tick(FPS)
